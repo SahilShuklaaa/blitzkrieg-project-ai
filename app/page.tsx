0d8efc33b5ef2ae46ce2dfdb6fc904704891dfc5
sahilshuklaaa <sahilshuklaaa@gmail.com>
2026-04-05 12:49:57 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Cool Shoes</h1>
+      <p>Best shoes in the market</p>
+
+      <button style={{ padding: "10px 20px", fontSize: "16px" }}>
+        Buy Now
+      </button>
+    </div>
+  );
+}
